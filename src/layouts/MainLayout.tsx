5d735c7edbd9575b53c5d2a2224366cfedb0effd
marcosengroff <marcosengroffm@gmail.com>
2026-07-27 15:26:47 -0300

    Ajusta texto do credito no rodape

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -163,7 +163,8 @@ export function MainLayout() {
         <footer className="px-4 pb-5 lg:px-8">
           <div className="flex justify-end">
             <div className="text-right text-sm font-medium text-slate-500">
-              <p>Desenvolvido por Marcos Engroff</p>
+              <p>Desenvolvido por</p>
+              <p>Marcos Engroff</p>
               <a
                 href="https://wa.me/5555999634342?text=Olá,%20Marcos!%20Estou%20utilizando%20o%20Restaurante%20Malaguetta%20e%20gostaria%20de%20falar%20com%20você."
                 target="_blank"
